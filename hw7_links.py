213ea6e3bee299f241368f0ac7a6c5b85d315d75
ANKITA PATRA <ap178152@gmail.com>
2026-04-06 16:16:15 +0000

    hw7links

---
+++ hw7_links.py
@@ -0,0 +1,21 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+BUCKET_NAME = "pagerank-bu-ap178152"
+PREFIX = "webgraph_v2/*"
+
+def run():
+    options = PipelineOptions()
+
+    with beam.Pipeline(options=options) as p:
+        (
+            p
+            | "Read from GCS" >> beam.io.ReadFromText(
+                f"gs://{BUCKET_NAME}/{PREFIX}"
+            )
+            | "Sample" >> beam.combiners.Sample.FixedSizeGlobally(5)
+            | "Print" >> beam.Map(print)
+        )
+
+if __name__ == "__main__":
+    run()
