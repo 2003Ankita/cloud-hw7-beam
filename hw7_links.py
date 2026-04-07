c7f81c0ac617c059d97ac638bb7503499a2b839a
ANKITA PATRA <ap178152@gmail.com>
2026-04-07 04:57:32 +0000

    Fix

--- hw7_links.py
+++ hw7_links.py
@@ -1,18 +1,25 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+import re
 
 BUCKET_NAME = "pagerank-bu-ap178152"
 PREFIX = "webgraph_v2/*"
 
+# Extract outgoing links
+def extract_outgoing(line):
+    # Example pattern: href="..."
+    links = re.findall(r'href="([^"]+)"', line)
+    return len(links)
+
 def run():
     options = PipelineOptions()
 
-    with beam.Pipeline(runner="DirectRunner", options=options) as p:
+    with beam.Pipeline(options=options) as p:
         (
             p
-            | "Read Local" >> beam.io.ReadFromText("sample_data/*.html")
-            | "Sample" >> beam.combiners.Sample.FixedSizeGlobally(5)
-            | "Flatten" >> beam.FlatMap(lambda x: x)
+            | "Read files" >> beam.io.ReadFromText(f"gs://{BUCKET_NAME}/{PREFIX}")
+            | "Count outgoing per line" >> beam.Map(lambda x: extract_outgoing(x))
+            | "Top 5 outgoing" >> beam.combiners.Top.Of(5)
             | "Print" >> beam.Map(print)
         )
 
