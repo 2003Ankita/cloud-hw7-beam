2fa2a6c4c91ec28d4fba4bb675cf90b9ec4863c1
ANKITA PATRA <ap178152@gmail.com>
2026-04-08 14:06:54 +0000

    Fix

--- hw7_links.py
+++ hw7_links.py
@@ -1,67 +1,65 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io import fileio
 import re
 import os
+from google.cloud import storage
 
-BUCKET_PATH = "gs://pagerank-bu-ap178152/webgraph_v2/1*.html"
+BUCKET_NAME = "pagerank-bu-ap178152"
+PREFIX = "webgraph_v2/"
 
-# Extract links from file content
-def extract_links(file):
-    content = file.read_utf8()
+def get_files():
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
 
-    filename = os.path.basename(file.metadata.path)
+    blobs = bucket.list_blobs(prefix=PREFIX)
 
-    # find all href links
+    data = []
+    for blob in blobs:
+        content = blob.download_as_text()
+        filename = os.path.basename(blob.name)
+        data.append((filename, content))
+
+    return data
+
+
+def extract_links(file_tuple):
+    filename, content = file_tuple
     links = re.findall(r'href="([^"]+)"', content)
-
-    # normalize links (just filenames like 123.html)
     links = [os.path.basename(link) for link in links]
-
     return (filename, links)
 
 
 def run():
     options = PipelineOptions([
-    "--runner=DirectRunner",
-    "--environment_type=LOOPBACK"
-])
+        "--runner=DirectRunner"
+    ])
+
+    files_data = get_files()   # 🔥 outside Beam (important)
 
     with beam.Pipeline(options=options) as p:
 
-        files = (
+        file_links = (
             p
-            | "Match Files" >> fileio.MatchFiles(BUCKET_PATH)
-            | "Read Files" >> fileio.ReadMatches()
+            | "Create" >> beam.Create(files_data)
+            | "Extract Links" >> beam.Map(extract_links)
         )
 
-        # (file, [outgoing_links])
-        file_links = files | "Extract Links" >> beam.Map(extract_links)
-
-        # -------------------------
-        # OUTGOING LINKS
-        # -------------------------
         outgoing = (
             file_links
             | "Outgoing Count" >> beam.Map(lambda x: (x[0], len(x[1])))
-            | "Top 5 Outgoing" >> beam.combiners.Top.Of(5, key=lambda x: x[1])
+            | "Top Outgoing" >> beam.combiners.Top.Of(5, key=lambda x: x[1])
         )
 
-        outgoing | "Print Outgoing" >> beam.Map(lambda x: print("Top Outgoing:", x))
+        outgoing | beam.Map(lambda x: print("Top Outgoing:", x))
 
-        # -------------------------
-        # INCOMING LINKS
-        # -------------------------
         incoming = (
             file_links
-            | "Create Incoming Pairs" >> beam.FlatMap(
-                lambda x: [(target, 1) for target in x[1]]
-            )
+            | "Incoming Pairs" >> beam.FlatMap(lambda x: [(t, 1) for t in x[1]])
             | "Sum Incoming" >> beam.CombinePerKey(sum)
-            | "Top 5 Incoming" >> beam.combiners.Top.Of(5, key=lambda x: x[1])
+            | "Top Incoming" >> beam.combiners.Top.Of(5, key=lambda x: x[1])
         )
 
-        incoming | "Print Incoming" >> beam.Map(lambda x: print("Top Incoming:", x))
+        incoming | beam.Map(lambda x: print("Top Incoming:", x))
 
 
 if __name__ == "__main__":
